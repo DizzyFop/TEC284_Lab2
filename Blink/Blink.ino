a99635e8316e0aa2fe08a9c6a59171aca9d271f1
Jack <54298425+DizzyFop@users.noreply.github.com>
2025-01-23 14:55:55 -0600

    Removed If statement and implemented for loop

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -24,20 +24,20 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  int timesToBlink = 1;
-  timesToBlink += 1;
-  if(timesToBlink > 4){
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(1000);                      // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-    delay(1000);                      // wait for a second
-  }
-  else{
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  }
-  
-  // initialize digital pin LED_BUILTIN as an output.
+    // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+
+  int timesToBlink = 1;
+  timesToBlink += 3;
+  
+  for(int i = 0; i <=timesToBlink; i++){
+      digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+      delay(1000);                      // wait for a second
+      digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+      delay(1000);                      // wait for a second
+  }
+
+    
 }
 
 // the loop function runs over and over again forever
