ab46c8647e535725c84923bcf5a181351e8674f1
Jack <54298425+DizzyFop@users.noreply.github.com>
2025-01-23 14:43:38 -0600

    Added code to make light stay on

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -32,6 +32,9 @@ void setup() {
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
     delay(1000);                      // wait for a second
   }
+  else{
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  }
   
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
